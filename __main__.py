50dc69ff1231e9f4dab69b93cdfcb436aafbfbfe
Emrana Begum <emrana97@gmail.com>
2024-05-23 22:00:28 -0400

    idk

--- __main__.py
+++ __main__.py
@@ -18,25 +18,6 @@ bot = commands.Bot(command_prefix="!", intents=intents)
 async def on_ready():
     print(f'We have logged in as {bot.user}')
 
-@bot.event
-async def on_message(message):
-    if message.author == bot.user:
-        return
-    
-
-    questions = ["do", "does", "am", "will", "is", "are", "can", "have", "should", "would", "could", "did", "shall"]
-    answers = ["Yes", "No", "For sure", "Perchance", "Perhaps", "Perhaps not", "Definitely not", "Nope", "Maybe", "Without a doubt", "Absolutely!", "Not a chance", "You bet!", "Never in a million years", "It's possible", "In your dreams", "Affirmative", "Negative", "Signs point to yes", "Don't count on it", "Ask again later", "My sources say no", "Outlook not so good", "Yes, but keep it quiet", "No, but nice try", "I can't say for sure", "Possibly", "Unlikely", "Very doubtful", "Absolutely not", "Yes, and it's amazing!", "No, but maybe later", "For sure, but don't tell anyone", "No way, Jose", "I'd say yes", "I'd say no", "Chances are slim", "It's a secret", "Most likely", "You wish", "As if", "Definitely yes", "Definitely maybe"]
-
-    for question in questions:
-        if message.content.startswith(question):
-            answer = random.choice(answers)
-            await message.channel.send(answer)
-
-    await bot.process_commands(message)
-
-@bot.command(name="hi")
-async def on_help(ctx):
-    await ctx.send("What would you like help with?")
 
 bot.load_extension("cogs.test")
 
