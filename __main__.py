c9953d8ca2ad473ea92a4a7d167e781d5f5a8784
Emrana Begum <emrana97@gmail.com>
2024-05-25 18:30:56 -0400

    added print statements for debugging

--- __main__.py
+++ __main__.py
@@ -1,27 +1,46 @@
+import asyncio
 import os
 from dotenv import load_dotenv
-import random
-
 import discord
 from discord.ext import commands
 
 # Load the environment variables from the .env file
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
+bot = commands.Bot(command_prefix="!", intents=discord.Intents.default())
 
-# Set up the bot with the necessary intents
-intents = discord.Intents.default()
-intents.message_content = True
 
-# Define the bot and the command prefix
-bot = commands.Bot(command_prefix="!", intents=intents)
+async def main():
 
-@bot.event
-async def on_ready():
-    print(f'We have logged in as {bot.user}')
+    @bot.event
+    async def on_ready():
+        print(f'We have logged in as {bot.user}!')
+        print("─────────────────────")
 
-# Load the cogs
-bot.load_extension("cogs.test")
+    for file in os.listdir("./cogs"):
+        if file.endswith(".py") and file!="__init__.py":
+            try:
+                cog = f"cogs.{file[:-3]}"
+                await bot.load_extension(cog)
+                print(f"Sucessfully loaded extension '{cog}'")
+                print("─────────────────────")
+            except Exception as e:
+                print(f"Failed to load etension '{cog}'. Error: {e}")
+                print("─────────────────────")
 
-# Run the bot with the specified token
-bot.run(TOKEN)
+
+    # Run bot
+    try:
+        print("Running bot")
+        print("─────────────────────")
+        await bot.start(TOKEN)
+        print("Bot Run successfully.")
+        print("─────────────────────")
+    except discord.LoginFailure:
+        print("Invalid token")
+        print("─────────────────────")
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
+        print("─────────────────────")
+
+asyncio.run(main())
