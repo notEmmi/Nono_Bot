fe81ee82f73935058ff4b8161409c7f0faf7b545
Emrana Begum <emrana97@gmail.com>
2024-05-25 21:15:43 -0400

    added greetings and it no work anymore :(

--- cogs/on_message.py
+++ cogs/on_message.py
@@ -17,26 +17,60 @@ class On_Message(commands.Cog):
     # Write Functions here
     @commands.Cog.listener()
     async def on_question(self, message):
-        questions = ["do", "does", "am", "will", "is", "are", "can", "have", "should", "would", "could", "did", "shall"]
-        answers = ["Yes", "No", "For sure", "Perchance", "Perhaps", "Perhaps not", "Definitely not", "Nope", "Maybe", "Without a doubt", "Absolutely!", "Not a chance", "You bet!", "Never in a million years", "It's possible", "In your dreams", "Affirmative", "Negative", "Signs point to yes", "Don't count on it", "Ask again later", "My sources say no", "Outlook not so good", "Yes, but keep it quiet", "No, but nice try", "I can't say for sure", "Possibly", "Unlikely", "Very doubtful", "Absolutely not", "Yes, and it's amazing!", "No, but maybe later", "For sure, but don't tell anyone", "No way, Jose", "I'd say yes", "I'd say no", "Chances are slim", "It's a secret", "Most likely", "You wish", "As if", "Definitely yes", "Definitely maybe"]
+
+        questions = ["do", "does", "am", "will",
+                     "is", "are", "can", "have",
+                     "should", "would", "could",
+                     "did", "shall"]
+        answers = ["Yes", "No", "For sure",
+                   "Perchance", "Perhaps",
+                   "Perhaps not", "Definitely not",
+                   "Nope", "Maybe", "Without a doubt",
+                   "Absolutely!", "Not a chance", "You bet!",
+                   "Never in a million years", "It's possible",
+                   "In your dreams", "Affirmative", "Negative",
+                   "Signs point to yes", "Don't count on it",
+                   "Ask again later", "My sources say no",
+                   "Outlook not so good", "Yes, but keep it quiet",
+                   "No, but nice try", "I can't say for sure",
+                   "Possibly", "Unlikely", "Very doubtful",
+                   "Absolutely not", "Yes, and it's amazing!",
+                   "No, but maybe later", "For sure, but don't tell anyone",
+                   "No way, Jose", "I'd say yes", "I'd say no",
+                   "Chances are slim", "It's a secret", "Most likely",
+                   "You wish", "As if", "Definitely yes", "Definitely maybe"]
 
         if message.author == self.bot.user:
             return
 
 
         for question in questions:
-            if message.content.startswith(question):
+            if message.content.lower().startswith(question):
                 answer = random.choice(answers)
                 await message.channel.send(answer)
                 break
 
-    @commands.Cog.listener()
-    async def on_greetings()
+    # @commands.Cog.listener()
+    # async def on_greetings(self, message):
+        
+    #     greetings = ['hello', 'hi', 'hey', 'good morning', 'good afternoon',
+    #                  'good evening', 'howdy', 'hiya', 'greetings', 'salutations']
+    #     goodbyes = ['goodbye', 'bye', 'farewell', 'see you later',
+    #                 'catch you later', 'take care', 'have a great day',
+    #                 'until next time','adios', 'so long']
 
-    @commands.command(name="hello")
-    async def on_hello(self, ctx):
-        await ctx.send("hello")
-
+        
+    #     if message.author == self.bot.user:
+    #         return
+        
+    #     for greeting in greetings:
+    #         if message.content.lower().startswith(greeting):
+    #             await message.channel.send(greeting + f" {message.author.mention}!")
+        
+    #     for bye in goodbyes:
+    #         if message.content.lower().startswith(bye):
+    #             await message.channel.send(bye + f" {message.author.mention}!")
+                
 
 
 async def setup(bot):
