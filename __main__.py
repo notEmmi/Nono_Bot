06306710d1d0b5e38dbfe78ac8b8fee53d361e35
Emrana Begum <emrana97@gmail.com>
2024-06-01 22:04:57 -0400

    added bot config, and comments

--- __main__.py
+++ __main__.py
@@ -6,10 +6,19 @@ from discord.ext import commands
 # Load the environment variables from the .env file
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
+
+# Define Intents
 intents = discord.Intents.default()
 intents.message_content = True
-bot = commands.Bot(command_prefix="!", intents=intents)
 
+# Create bot
+bot = commands.Bot(command_prefix="!",
+                   intents=intents,
+                   allowed_mentions = discord.AllowedMentions(everyone=False, users=True, roles=False)
+)
+
+
+# Load cogs
 async def load_cogs():
     for file in os.listdir("./cogs"):
         if file.endswith(".py") and file != "__init__.py":
@@ -20,9 +29,12 @@ async def load_cogs():
             except Exception as e:
                 print(f"Failed to load extension '{cog}'. Error: {e}")
 
+# Ready message on startup
 @bot.event
 async def on_ready():
     await load_cogs()
     print(f"We have logged in as {bot.user}!")
 
+
+# Run bot
 bot.run(TOKEN)
